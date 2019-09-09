300a9c8acc3d6479b016976a110cea63f4bca86a
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-09-09 16:52:14 +0500

    scalar transformation added

--- support_vector_regression.py
+++ support_vector_regression.py
@@ -23,12 +23,12 @@ y = dataset.iloc[:, 2].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
 
 # Fitting the Regression Model to the dataset
 # Create your regressor here

417a01d57950c82d2ae4841573fae42105b80933
Muhammad Moaz Khan <moazkh60@gmail.com>
2019-09-09 17:04:43 +0500

    Salary of employee at 6.5 level predicted

--- support_vector_regression.py
+++ support_vector_regression.py
@@ -16,7 +16,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+y = dataset.iloc[:, 2:3].values
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
@@ -32,17 +32,20 @@ y = sc_y.fit_transform(y)
 
 # Fitting the Regression Model to the dataset
 # Create your regressor here
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X,y)
 
 # Predicting a new result
-y_pred = regressor.predict(6.5)
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
 
 # Visualising the Regression results
-plt.scatter(X, y, color = 'red')
+"""plt.scatter(X, y, color = 'red')
 plt.plot(X, regressor.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
-plt.show()
+plt.show()"""
 
 # Visualising the Regression results (for higher resolution and smoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
